1e43803c797fcfbf45eb049b5a21faa84402e74d
Rohan Kumar <rohaan@redhat.com>
2021-07-14 22:16:50 +0530

    Initial Commit

---
+++ src/test/java/io/fabric8/demo/Ipv6ClusterTest.java
@@ -0,0 +1,25 @@
+package io.fabric8.demo;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.VersionInfo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class Ipv6ClusterTest {
+    @Test
+    void testClientConnectsInIpv6Cluster() {
+        // Given
+        KubernetesClient client = new DefaultKubernetesClient();
+
+        // When
+        VersionInfo versionInfo = client.getVersion();
+
+        // Then
+        assertNotNull(versionInfo);
+        assertEquals("1", versionInfo.getMajor());
+        assertEquals("21", versionInfo.getMinor());
+    }
+}
